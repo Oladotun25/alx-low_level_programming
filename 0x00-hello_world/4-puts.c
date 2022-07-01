06d5a1c87be20e76aa0c90c67d468800cc002b2d
Oladotun25 <osadotunsamuel25@gmail.com>
2022-07-01 15:13:41 -0700

    great fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * print to output
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
