0bc01c0e3b5125d262e3ec38a1ddf855d21b65ac
Oladotun25 <osadotunsamuel25@gmail.com>
2022-07-05 14:04:37 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Prints putchar
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	
 	return (0);
 }
