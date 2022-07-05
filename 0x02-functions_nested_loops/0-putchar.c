41b969866cc447720e6491ba330376568b017201
Oladotun25 <osadotunsamuel25@gmail.com>
2022-07-05 15:06:06 -0700

    oputchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
-#include "main.h"
+#include "main.h";
 
-/**
- * main - Prints putchar
+/** main - Prints putchar
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,6 +14,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
