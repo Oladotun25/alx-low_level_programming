77bb6cacf9e95379a6afbe7335a3a61bd234de55
Oladotun25 <osadotunsamuel25@gmail.com>
2022-07-13 14:43:36 -0700

    pointer rsetting

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 -> a function for reference pointer updating
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
  * @n: pointer to n to be updated
  */
 void reset_to_98(int *n)
