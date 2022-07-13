46f427ed4625da70e88cde3f97758fc7010130c5
Oladotun25 <osadotunsamuel25@gmail.com>
2022-07-13 14:52:58 -0700

    quick fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * 0-reset_to_98 - Takes a pointer to an int and updates
+ * reset_to_98 - Takes a pointer to an int and updates
  *               the value it points to to 98.
  * @n: The pointer to an int.
  */
