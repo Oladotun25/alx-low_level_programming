d52a72362f64743f6a221fdf19d819d5f1f450d0
Oladotun25 <osadotunsamuel25@gmail.com>
2022-07-13 20:22:52 -0700

    quick

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing declarations for all functions
- * 	used in the 0x02-functions_nested_loops directory.
- */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-int _putchar(char c);
+char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif /* HOLBERTON_H */
+
